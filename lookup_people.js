8acdacd9378e18ef5a5545ab96fa4e17b0e17a8a
Kaush <kaush.jonna@gmail.com>
2019-02-26 10:32:12 -0500

    completed creating a query file

---
+++ lookup_people.js
@@ -0,0 +1,31 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const person = process.argv.splice(2); //input
+
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE first_name= $1::text`, [person[0]], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(`Found ${result.rows.length} Person(s) by the name ${person[0]}:`);
+    let i = 1;
+    result.rows.forEach((item) => {
+      console.log(`- ${i}: ${item.first_name} ${item.last_name}, born '${item.birthdate}`);
+      i++;
+    });
+    client.end();
+  });
+});
