7e38a35e13ff8b453f7eaad6ddced9c8c56c58ed
Kaush <kaush.jonna@gmail.com>
2019-02-26 12:14:56 -0500

    added last name lookup functionality

--- lookup_people.js
+++ lookup_people.js
@@ -17,15 +17,32 @@ client.connect((err) => {
     return console.error("Connection Error", err);
   }
   client.query(`SELECT * FROM famous_people WHERE first_name= $1::text`, [person[0]], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
+    if (result.rows.length === 0) {
+      console.log('checking last name...');
+      client.query(`SELECT * FROM famous_people WHERE last_name= $1::text`, [person[0]], (err, result) => {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        console.log(`Found ${result.rows.length} Person(s) by the name ${person[0]}:`);
+        let i = 1;
+        result.rows.forEach((item) => {
+          console.log(`- ${i}: ${item.first_name} ${item.last_name}, born '${item.birthdate}`);
+          i++;
+        });
+        client.end();
+      });
+
+    } else {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      console.log(`Found ${result.rows.length} Person(s) by the name ${person[0]}:`);
+      let i = 1;
+      result.rows.forEach((item) => {
+        console.log(`- ${i}: ${item.first_name} ${item.last_name}, born '${item.birthdate}`);
+        i++;
+      });
+      client.end();
     }
-    console.log(`Found ${result.rows.length} Person(s) by the name ${person[0]}:`);
-    let i = 1;
-    result.rows.forEach((item) => {
-      console.log(`- ${i}: ${item.first_name} ${item.last_name}, born '${item.birthdate}`);
-      i++;
-    });
-    client.end();
   });
 });
